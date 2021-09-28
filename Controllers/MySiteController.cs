e8fb74176c70e65bb8cc9ca6a46d59f6a786535d
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-09-28 08:53:44 -0400

    Initial commit

---
+++ Controllers/MySiteController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab9_2_MVC.Controllers
+{
+    public class MySiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyPage()
+        {
+            ViewData["FirstName"] = "Jeremy";
+            ViewData["LastName"] = "Jones";
+            return View();
+        }
+
+        public IActionResult MyForm()
+        {
+            return View();
+        }
+        public IActionResult ThankYou(string FirstName, string LastName, string Email)
+        {
+            ViewData["FirstName"] = FirstName;
+            ViewData["LastName"] = LastName;
+            ViewData["Email"] = Email;
+            return View();
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab9_2_MVC.Models
+{
+    public class Players
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
